fa946909e508956ff2bbca197ee5dcea65cf1d55
egor <egor.popov8086@gmail.com>
2022-09-08 23:49:14 +0500

    first commit

---
+++ Dz_1/Program.cs
@@ -0,0 +1,94 @@
+﻿//Задача №2
+
+
+// Console.Clear();
+// Console.WriteLine("Введите первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Clear();
+
+
+// if(number1 > number2)
+// {
+//     Console.WriteLine("Наибольшее число:");
+//     Console.WriteLine(number1);
+//     Console.WriteLine("Наименьшее число:");
+//     Console.WriteLine(number2);
+// }
+// if(number1 < number2)
+// {
+//     Console.WriteLine("Наибольшее число:");
+//     Console.WriteLine(number2);
+//     Console.WriteLine("Наименьшее число:");
+//     Console.WriteLine(number1);
+// }
+// else
+// {
+//     Console.WriteLine("Эти числа равны");
+// }
+
+
+
+
+// Задача №4
+
+
+// Console.Clear();
+// Console.WriteLine("Введите первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите третье число");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// int[] i = { number1, number2, number3 };
+// int max = i.Max(); //Подсмотрел данные две строчки в интернете
+
+// Console.Clear();
+// Console.WriteLine("Максимальное число:");
+// Console.WriteLine(max);
+
+
+
+//Задача №6
+
+
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number % 2==0)
+// {
+//     Console.WriteLine("Число чётное");
+// }
+// else
+// {
+//     Console.WriteLine("Число нечётное");
+// }
+
+
+
+
+// Задача №8
+
+
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// Console.WriteLine("Чётные числа:");
+// while(0 < number)
+// {
+//     if(number % 2==0)
+//     {
+
+//         Console.WriteLine(number);
+        
+//     }
+//     number--;
+// }
---
+++ Seminar_1/Program.cs
@@ -0,0 +1,79 @@
+﻿// Console.WriteLine("Введите число");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sqr = number * number;
+
+// Console.WriteLine(sqr);
+
+// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+
+// Console.WriteLine("Введите число №1");
+// int number1= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число №2");
+// int number2= Convert.ToInt32(Console.ReadLine());
+
+// if (number1 == number2*number2)
+// {
+//     Console.WriteLine("Число 1 является квадратом");
+// }
+// else 
+// {
+//     Console.WriteLine("Число 1 не является квадратом");
+// }
+
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру
+
+// Console.WriteLine("Введите число");
+// int numberDay= Convert.ToInt32(Console.ReadLine());
+
+// if(numberDay<1 || numberDay>7)
+// {
+//     Console.WriteLine("Ошибку");
+// }
+// if(numberDay==1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// if(numberDay==2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// if(numberDay==3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// if(numberDay==4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// if(numberDay==5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// if(numberDay==6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// if(numberDay==7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int numberNeg = number*(-1);
+// Console.Clear();
+// while(numberNeg<=number)
+// {
+//     Console.WriteLine(numberNeg);
+//     numberNeg++;
+
+// }
+
+// Console.WriteLine("Введите любое число, а мы определим его последнюю цифру");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int number1 = number%10;
+// Console.Clear();
+// Console.WriteLine("Ваш ответ:");
+// Console.WriteLine(number1);
