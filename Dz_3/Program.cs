deb76e62601e4d3ec404dc7daa9e3bb775c169fb
egor <egor.popov8086@gmail.com>
2022-09-15 18:57:41 +0500

    commit 3

---
+++ Dz_3/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+ 
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num = new Random().Next(10000, 100000);
+// Console.WriteLine(num);
+
+// int a = num;
+// int b = 0;
+// while(num > 0)
+// {
+//     int c = num % 10;
+//     b = b * 10 + c;
+//     num = num / 10;
+// }
+// if(a == b)
+// {
+//     Console.WriteLine("Число является палиндромом");
+// }
+// else
+// {
+//     Console.WriteLine("Число не является палиндромом");
+// }
+
+
+
+// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Console.WriteLine("Введите Х1:");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Y1:");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Z1:");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Х2:");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Y2:");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Z2:");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+
+// Console.WriteLine($"Расстояние между точками:{d}");
+
+
+
+
+//Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while(count <= num)
+// {
+//     Console.WriteLine(Math.Pow(count,3));
+//     count++;
+// }
+
---
+++ Seminar_3/Program.cs
@@ -0,0 +1,127 @@
+﻿// string str = Console.ReadLine();
+
+
+// if(str.Length<=2)
+// {
+//     Console.WriteLine("Данное число не имеет третьей цифры");
+//     return;
+// }
+
+// Console.WriteLine(str[2]);
+
+
+// int testNumber = 4356;
+
+// Console.WriteLine(testNumber==1);
+
+
+// Console.WriteLine("Введите координату: x");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату: y");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine();
+
+// if(x>0&&y>0)
+// {
+//     Console.WriteLine("1");
+// }
+
+// if(x<0&&y>0)
+// {
+//     Console.WriteLine("2");
+// }
+
+// if(x<0&&y<0)
+// {
+//     Console.WriteLine("3");
+// }
+
+// if(x>0&&y<0)
+// {
+//     Console.WriteLine("4");
+// }
+
+// if(x==0||y==0)
+// {
+//     Console.WriteLine("Определить нельзя");
+// }
+
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+
+// Console.WriteLine("Введите номер четверти:");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number == 1)
+// {
+//     Console.WriteLine("Значение х: от 0 до +бесконечности; Значение у: от 0 до +бесконечности");
+// }
+// if(number == 2)
+// {
+//     Console.WriteLine("Значение х: от 0 до -бесконечности; Значение у: от 0 до +бесконечности");
+// }
+// if(number == 3)
+// {
+//     Console.WriteLine("Значение х: от 0 до -бесконечности; Значение у: от 0 до -бесконечности");
+// }
+// if(number == 4)
+// {
+//     Console.WriteLine("Значение х: от 0 до +бесконечности; Значение у: от 0 до -бесконечности");
+// }
+// else
+// {
+//     Console.WriteLine("Error");
+// }
+
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+
+
+// Console.WriteLine("Введите Х1:");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Y1:");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Х2:");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Y2:");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+
+// // int x = x1*x1;
+// int x = Math.Pow = (x1, 2);
+
+// double x = 51/7;
+// double y = Math.Sqrt(49);
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while(count <= num)
+// {
+//     Console.WriteLine(Math.Pow(count,3));
+//     count++;
+// }
+
+
+
+
+
+
+
+
+
+
