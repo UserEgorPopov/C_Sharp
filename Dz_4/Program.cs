c92d7a267e35ef16faa6635d0786e54abe5b3871
egor <egor.popov8086@gmail.com>
2022-09-19 22:14:37 +0500

    commit 4

---
+++ Dz_4/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+// Console.WriteLine("Введите первое число:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int num3 = num1;
+// while (num2 > 1)
+// {
+//     num2 = num2 - 1;
+//     num1 = num1 * num3;
+// }
+
+// Console.WriteLine("Результат:");    
+// Console.WriteLine(num1);
+
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// Console.WriteLine("Введите число");
+// int a = (Convert.ToInt32(Console.ReadLine()));
+// int b = 0;
+//     while (a > 0)
+//     {
+ 
+//         b = b + a % 10;
+//         a = a /10 ;
+ 
+//     }
+// Console.WriteLine("Сумма цифр числа:");
+// Console.WriteLine(b);
+
+
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+
+// int[] array = new int [8];
+
+
+// void FillArray(int[] collection) 
+// {
+//     int Length = collection.Length;
+//     int index = 0;
+//     while (index < Length)
+//     {
+//         collection[index] = new Random().Next(0,1000);
+//         index++;
+//     }
+// }
+
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while(position < count)
+//     {
+//         Console.Write(col[position] + ",");
+//         position++;
+//     }
+// }
+
+// FillArray(array);
+// PrintArray(array);
---
+++ Seminar_4/Program.cs
@@ -0,0 +1,120 @@
+﻿// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Сумма ="+GetSum(num));
+
+// int GetSum(int number)
+// {
+//     int sum = 0;
+//     int count = 0;
+
+//     while(number>count)
+//     {
+//         count++;
+//         sum+=count;
+
+        
+//     }
+//     return sum;
+
+// }
+
+
+
+
+// Console.WriteLine("Write number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Col-vo cifr:" +GetCount(num));
+
+// int GetCount(int number)
+// {
+//     int count = 0;
+//     while(number>0)
+//     {
+//         count++;
+//         number/=10;
+//     }
+//     return count;
+
+// }
+
+
+
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+// int GetProduct(int num)
+// {
+//     int prod = 1;
+//     for(int i = 1; i <= num; i++)
+//     {
+//         prod*=num;
+//     }
+//     return prod;
+// }
+
+// Console.WriteLine(GetProduct(num));
+
+
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(GetSum(num));
+
+// int GetSum(int number)
+// {
+//     int pr =1; 
+//     int count = 0;
+//     while(number > count)
+//     {
+//         count++;
+//         pr *= count;
+//     }
+//     return pr;
+// }
+
+
+// int []array = GetArray(8);
+// Console.WriteLine(array[]);
+
+// int [] GetArray(int size)
+// {
+//     int [] result = new int [size];
+//     for(int i = 0; i< size; i++)
+//     {
+//         result[i] = new Random().Next(0,2);
+
+         
+//     }
+//     return result;
+
+// }
+
+// int[] array = new int [8];
+
+
+// void FillArray(int[] collection) 
+// {
+//     int Length = collection.Length;
+//     int index = 0;
+//     while (index < Length)
+//     {
+//         collection[index] = new Random().Next(0,2);
+//         index++;
+//     }
+// }
+
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while(position < count)
+//     {
+//         Console.Write(col[position] + ",");
+//         position++;
+//     }
+// }
+
+// FillArray(array);
+// PrintArray(array);
+
+
